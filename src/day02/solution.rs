e25588609a5df92d585fbb9dce509283fa0c64bd
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2021-12-01 23:19:35 -0600

    Day 2 ✅

---
+++ src/day02/solution.rs
@@ -0,0 +1,38 @@
+pub fn part1(input: String) -> i32 {
+    let mut depth = 0;
+    let mut pos = 0;
+    input
+        .lines()
+        .for_each(|line| {
+            let split = line.split(' ').collect::<Vec<&str>>();
+            match split[0] {
+                "up" => depth -= split[1].parse::<i32>().unwrap(),
+                "down" => depth += split[1].parse::<i32>().unwrap(),
+                "forward" => pos += split[1].parse::<i32>().unwrap(),
+                _ => panic!("Unknown direction"),
+            }
+        });
+    return depth * pos;
+}
+
+pub fn part2(input: String) -> i32 {
+    let mut depth = 0;
+    let mut pos = 0;
+    let mut aim = 0;
+    input
+        .lines()
+        .for_each(|line| {
+            let split = line.split(' ').collect::<Vec<&str>>();
+            match split[0] {
+                "up" => aim -= split[1].parse::<i32>().unwrap(),
+                "down" => aim += split[1].parse::<i32>().unwrap(),
+                "forward" => {
+                    let x = split[1].parse::<i32>().unwrap();
+                    pos += x;
+                    depth += aim * x;
+                },
+                _ => panic!("Unknown direction"),
+            }
+        });
+    return depth * pos;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use std::{env, fs};
 
 #[path = "day01/solution.rs"] mod day01;
+#[path = "day02/solution.rs"] mod day02;
 // PATH_MARKER
 
 type PartFunction = fn(String) -> i32;
 
-fn get_day_fns() -> (PartFunction, PartFunction) {
+fn get_day_num() -> u8 {
     let args = env::args().collect::<Vec<String>>();
     if args.len() < 2 {
         panic!("Must provide a day number as a argument to the program");
     }
     let day_string = args[1].trim().to_string();
-    let day_num: u8 = match day_string.parse() {
+    return match day_string.parse() {
         Ok(num) => num,
         Err(_) => {
             panic!("Invalid number: {}", day_string);
         }
     };
+}
+
+fn get_day_fns() -> (PartFunction, PartFunction) {
+    let day_num = get_day_num();
     match day_num {
         1 => return (day01::part1, day01::part2),
+        2 => return (day02::part1, day02::part2),
 // CASE_MARKER
         _ => {
             panic!("Invalid day number: {}", day_num);
@@ -27,7 +33,7 @@ fn get_day_fns() -> (PartFunction, PartFunction) {
 }
 
 fn main() {
-    let contents = fs::read_to_string("src/day01/input.txt").expect("Failed to read puzzle input file");
+    let contents = fs::read_to_string(format!("src/day{:02}/input.txt", get_day_num())).expect("Failed to read puzzle input file");
     let (part1, part2) = get_day_fns();
     println!("Part 1: {}", part1(contents.clone()));
     println!("Part 2: {}", part2(contents.clone()));
