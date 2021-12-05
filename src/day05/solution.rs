9e1458575443a4db00b15786b5d5de93c4c634be
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2021-12-05 00:02:37 -0600

    Remove redundant loops

--- src/day05/solution.rs
+++ src/day05/solution.rs
@@ -5,17 +5,13 @@ fn point_count(input: String, diagonals: bool) -> i32 {
     let mut points = HashMap::new();
     input
         .lines()
-        .map(|line| {
+        .for_each(|line| {
             let re = Regex::new(r"^(\d+),(\d+) -> (\d+),(\d+)$").unwrap();
             let caps = re.captures(line).unwrap();
-            (
+            let ((ax, ay), (bx, by)) = (
                 (caps[1].parse::<i32>().unwrap(), caps[2].parse::<i32>().unwrap()),
                 (caps[3].parse::<i32>().unwrap(), caps[4].parse::<i32>().unwrap()),
-            )
-        })
-        .filter(|&((ax, ay), (bx, by))| ax == bx || ay == by || (diagonals && ((ay - by) / (ax - bx)).abs() == 1))
-        .for_each(|((ax, ay), (bx, by))| {
-            // println!("{:?} -> {:?}", (ax, ay), (bx, by));
+            );
             if ax == bx {
                 // Vertical
                 for y in std::cmp::min(ay, by)..=std::cmp::max(ay, by) {
@@ -26,7 +22,7 @@ fn point_count(input: String, diagonals: bool) -> i32 {
                 for x in std::cmp::min(ax, bx)..=std::cmp::max(ax, bx) {
                     *(points.entry((x, ay)).or_insert(0)) += 1;
                 }
-            } else {
+            } else if diagonals && ((ay - by) / (ax - bx)).abs() == 1 {
                 // Diagonals
                 let m = (ay - by) / (ax - bx);
                 let b = ay - m * ax;
